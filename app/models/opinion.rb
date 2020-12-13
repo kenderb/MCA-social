687acc73d57dd64a671c75a22c8dfc47251b85b6
kenderb <kenderbolivar@gmail.com>
2020-12-13 18:38:42 -0500

    Add opinion validations and connection to the user model

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,2 +1,7 @@
 class Opinion < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  validates_presence_of :text
+  validates :text, length: { minimum: 10, maximum: 120 }
+
+  scope :ordered_by_most_recent, -> { includes(:author, :likes).order(created_at: :desc) }
 end
