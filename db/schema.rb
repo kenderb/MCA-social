7c7b03177b680866ca241c1683a3f0e6f3a3640f
kenderb <kenderbolivar@gmail.com>
2020-12-13 18:11:52 -0500

    Create the followings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_13_215144) do
+ActiveRecord::Schema.define(version: 2020_12_13_231009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "followings", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "opinions", force: :cascade do |t|
     t.integer "autor_id", null: false
