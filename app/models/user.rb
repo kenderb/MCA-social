190e3151f204d3e755e4d8295ff7350623ef255e
kenderb <kenderbolivar@gmail.com>
2020-12-13 18:36:07 -0500

    Add more validations for the user and queries

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,18 @@ class User < ApplicationRecord
   has_attached_file :cover_image, styles: { large: '800x300>' }
   validates_attachment_content_type :cover_image, content_type: %r{\Aimage/.*\z}
 
+  validates_uniqueness_of :username, case_sensitive: false, message: 'Username already taken.'
+  validates :username, length: { minimum: 4, too_short: 'Name too short' }
+  validates :fullname, length: { minimum: 6, maximum: 20, too_short: 'Full name has to be longer'}
+  validates_presence_of :username, message: 'User name cannot be blank'
+  validates_presence_of :fullname, message: 'Full name cannot be blank'
+
   has_many :opinions, foreign_key: 'author_id', dependent: :destroy
   has_many :followings, foreign_key: 'follower_id', dependent: :destroy
   has_many :follows, through: :followings, source: :followed
   has_many :inverse_followings, class_name: 'Following', foreign_key: 'followed_id'
   has_many :followers, through: :inverse_followings
+  def friends_not_following
+    User.where.not(id: follows).where.not(id: id).order('created_at DESC')
+  end
 end
