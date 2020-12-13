cf9eb50bb6ea54b47291164c4d087c17287dc54d
kenderb <kenderbolivar@gmail.com>
2020-12-13 18:23:45 -0500

    Add validation for images

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  has_attached_file :photo, styles: { thumb: '60x60#', medium: "150x150>", large: '250x250' }
+  validates_attachment_content_type :photo, content_type: %r{\Aimage/.*\z}
+
+  has_attached_file :cover_image, styles: { large: '800x300>' }
+  validates_attachment_content_type :cover_image, content_type: %r{\Aimage/.*\z}
+
   has_many :opinions, foreign_key: 'author_id', dependent: :destroy
   has_many :followings, foreign_key: 'follower_id', dependent: :destroy
   has_many :follows, through: :followings, source: :followed
