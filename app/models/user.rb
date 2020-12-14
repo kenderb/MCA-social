bd582802e2447d7e3d0d391c7449666217b47e62
kenderb <kenderbolivar@gmail.com>
2020-12-14 02:10:36 -0500

    Configure ImageMagick

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_attached_file :photo, styles: { medium: "250x250>", thumb: "60x60#" }, default_url: "/images/default_photo.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :cover_image, styles: { medium: "300x300>" }, default_url: "/images/default_cover.png"
+  has_attached_file :cover_image, styles: { medium: "800x300>" }, default_url: "/images/default_cover.png"
   validates_attachment_content_type :cover_image, content_type: /\Aimage\/.*\z/
 
   validates_uniqueness_of :username, case_sensitive: false, message: 'Username already taken.'
