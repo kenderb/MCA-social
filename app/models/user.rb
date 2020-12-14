11d007239919f1e00b6f331f9cfefcc02360e20c
kenderb <kenderbolivar@gmail.com>
2020-12-14 00:18:52 -0500

    Change attachment styles properties

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_attached_file :photo, styles: { thumb: '60x60#', medium: "150x150>", large: '250x250' }
+  has_attached_file :photo, styles: { medium: "250x250>", thumb: "60x60#" }, default_url: "/images/default_photo.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :cover_image, styles: { large: '800x300>' }
+  has_attached_file :cover_image, styles: { medium: "300x300>" }, default_url: "/images/default_cover.png"
   validates_attachment_content_type :cover_image, content_type: /\Aimage\/.*\z/
 
   validates_uniqueness_of :username, case_sensitive: false, message: 'Username already taken.'
