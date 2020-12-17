d5325dbe2b145041c2a7143d2f9e2f1c4b4ac43c
kenderb <kenderbolivar@gmail.com>
2020-12-16 20:57:36 -0500

    Finish the style for the form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,21 @@
 class SessionsController < ApplicationController
   def new
-    
   end
-  
+
   def create
     @user = User.find_by(username: params[:session][:username].strip.downcase)
-
     if @user
       session[:username] = @user.username
-      flash[:success] = 'Welcome back.'
-      redirect_to root_path
+      redirect_to root_path, notice: 'Logged in successfully!'
     else
-      flash.now[:alert] = 'The username doesn’t match any account.'
-      redirect_to login_path
+      flash.now[:alert] = 'Email incorrect'
+      render :new
     end
   end
-  
+
   def destroy
     session[:username] = nil
     flash[:success] = "You’re logged out."
     redirect_to login_path
   end
-  
-  
 end
--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,7 +1,7 @@
 class Opinion < ApplicationRecord
   belongs_to :author, class_name: 'User'
   validates_presence_of :text
-  validates :text, length: { minimum: 10, maximum: 120 }
+  validates :text, length: { minimum: 10, maximum: 120, message: 'Opinion minmun of 10 maximun of 120.' }
   has_many :likes, dependent: :destroy
 
   scope :ordered_by_most_recent, -> { includes(:author, :likes).order(created_at: :desc) }
