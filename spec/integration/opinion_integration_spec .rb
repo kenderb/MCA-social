7681dcb9829fdf34bebb483f1afe7fe05577a9d3
kenderb <kenderbolivar@gmail.com>
2020-12-18 13:40:47 -0500

    Add opinion integration tests

---
+++ spec/integration/opinion_integration_spec .rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe Opinion, type: :feature do
+
+  scenario 'Create a opinion successfully' do
+    visit new_user_path
+    fill_in 'Username', with: 'Juanito'
+    fill_in 'Fullname', with: 'Juanito Bailador'
+    click_on 'Create User'
+    fill_in 'Enter the movie name', with: 'IT'
+    fill_in 'Enter your opinion', with: 'Some opinion for the movie'
+    click_on 'Create Opinion'
+    expect(page).to have_content('Your opinion has been posted')
+  end
+
+  scenario 'not Create a opinion successfully' do
+    visit new_user_path
+    fill_in 'Username', with: 'Juanito'
+    fill_in 'Fullname', with: 'Juanito Bailador'
+    click_on 'Create User'
+    fill_in 'Enter the movie name', with: ''
+    fill_in 'Enter your opinion', with: 'Some opinion for the movie'
+    click_on 'Create Opinion'
+    expect(page).to have_content('Something went wrong')
+  end
+end
