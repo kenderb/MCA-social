b5adbe4e55cd2f10a6da907399b2e949ea91a4ad
kenderb <kenderbolivar@gmail.com>
2020-12-18 13:32:09 -0500

    Add count_opinion and opinion scope tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 
 RSpec.describe User, type: :model do
   subject { User.new(username: 'Kender', fullname: 'Kender Bolivar') }
+  
+
   describe 'associations' do
     it { should have_many(:opinions) }
     it { should have_many(:followings) }
@@ -11,6 +13,7 @@ RSpec.describe User, type: :model do
     it { should have_many(:followers) }
     it { should have_many(:likes) }
   end
+
   describe 'validations' do
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:fullname) }
@@ -18,4 +21,15 @@ RSpec.describe User, type: :model do
     it { should validate_length_of(:fullname) }
     it { should validate_length_of(:username) }
   end
+
+  describe "friends_not_following" do
+    it "show not following friend" do
+      user1 = User.new(username: 'userone', fullname: 'user one name')
+      user2 = User.new(username: 'usertwo', fullname: 'user two name')
+      user1.save
+      user2.save
+      expect(user1.friends_not_following[0].username).to include(user2.username)
+    end
+  end
+  
 end
