3e54f11b93c264351246c2e8f94184e62f775fea
kenderb <kenderbolivar@gmail.com>
2020-12-18 11:37:54 -0500

    Finish the opinions form

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,7 +1,8 @@
 class Opinion < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  validates_presence_of :text, :movie_name
-  validates :text, length: { minimum: 10, maximum: 120, message: 'Opinion minmun of 10 maximun of 120.' }
+  validates_presence_of :text, message: "can't be blank"
+  validates_presence_of :movie_name, message: "can't be blank"
+  validates :text, length: { minimum: 10, maximum: 120, too_short: 'Opinion too short' }
   validates :movie_name, length: { minimum: 2, maximum: 30, message: 'movie_name minmun of 2 maximun of 30.' }
   has_many :likes, dependent: :destroy
 
