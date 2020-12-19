a6ccf3221d859338d0c604cc6bfe8333fb52a22b
kenderb <kenderbolivar@gmail.com>
2020-12-18 19:48:19 -0500

    Remove some files

--- config/cloudinary.rb
+++
@@ -1,7 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = 'hdbnc6ura'
-  config.api_key = ENV["CL_ID"]
-  config.api_secret = ENV["CL_KEY"]
-  config.secure = true
-  config.cdn_subdomain = true
-end
